984447e475e56771cd190e81baa6ae817458fb69
michaelni678 <michael.ni678@gmail.com>
2024-01-28 15:58:55 -0500

    gfx example

--- util/engine/examples/gfx.rs
+++ util/engine/examples/gfx.rs
@@ -1,99 +1,138 @@
 use beyond_engine::{
-  include_wrt_manifest, Camera, Color, Mesh, Point, Renderable, Renderer, Texture, Transform, World,
-};
-use glium::backend::glutin::SimpleWindowBuilder;
-use winit::{
-  event::{Event, WindowEvent},
-  event_loop::EventLoop,
+  include_wrt_manifest, Renderer, World,
+  App, AppEventHandler, AppSetupHandler, AppWindowEventHandler, Basics, Camera, Color, CommandQueue, Context, EngineError, LoadScene, Mesh, Point, Renderable, Scene, Scenes, Texture, Transform, WindowBuilder, ELWT
 };
 
-fn main() {
-  // Application setup.
-  let event_loop = EventLoop::new().unwrap();
-  let (window, display) = SimpleWindowBuilder::new()
-    .with_title("Beyond Engine Graphics Example")
-    .build(&event_loop);
-  // Create the renderer.
-  let mut renderer = Renderer::new(display).unwrap();
-  // Add the samplers.
-  renderer
-    .add_sampler(
-      include_wrt_manifest!("/examples/standalone.png"),
-      [(
-        "Standalone",
-        vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]],
-      )],
-    )
-    .unwrap();
-  renderer
-    .add_sampler(
-      include_wrt_manifest!("/examples/atlas.png"),
-      [
-        (
-          "Atlas Full",
+/// An example application.
+pub struct ExampleApp;
+
+impl App for ExampleApp {}
+
+impl AppSetupHandler for ExampleApp {
+  fn window(window_builder: WindowBuilder) -> WindowBuilder {
+    window_builder.with_title("Beyond Engine Graphics Example")
+  }
+  fn scenes(scenes: Scenes) -> Scenes {
+    scenes.register(ExampleScene)
+  }
+}
+
+impl AppEventHandler for ExampleApp {
+  fn init(
+    &mut self,
+    command_queue: &mut CommandQueue,
+    context: &mut Context,
+  ) -> Result<(), EngineError> {
+    // Load the example scene.
+    command_queue.enqueue(LoadScene::<ExampleScene>);
+    // Add the samplers.
+    context.renderer
+      .add_sampler(
+        include_wrt_manifest!("/examples/standalone.png"),
+        [(
+          "Standalone",
           vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]],
-        ),
-        (
-          "Atlas 0",
-          vec![[0.0, 0.0], [0.5, 0.0], [0.5, 0.5], [0.0, 0.5]],
-        ),
-        (
-          "Atlas 1",
-          vec![[0.0, 0.5], [0.5, 0.5], [0.5, 1.0], [0.0, 1.0]],
-        ),
-        (
-          "Atlas 2",
-          vec![[0.5, 0.0], [1.0, 0.0], [1.0, 0.5], [0.5, 0.5]],
-        ),
-        (
-          "Atlas 3",
-          vec![[0.5, 0.5], [1.0, 0.5], [1.0, 1.0], [0.5, 1.0]],
-        ),
-      ],
-    )
-    .unwrap();
-  // Create the world.
-  let mut world = World::new();
-  // Spawn the active entity.
-  // Solid red renderable.
-  let entity = world.spawn_entity((
-    Transform::new([0.0, 0.0], [128.0, 128.0]),
-    Renderable::new(
-      Color::rgb(1.0, 0.0, 0.0),
-      Texture::none(),
-      Mesh::new(
-        vec![
-          Point::new(-0.5, -0.5),
-          Point::new(0.5, -0.5),
-          Point::new(0.5, 0.5),
-          Point::new(-0.5, 0.5),
+        )],
+      )
+      .unwrap();
+    context.renderer
+      .add_sampler(
+        include_wrt_manifest!("/examples/atlas.png"),
+        [
+          (
+            "Atlas Full",
+            vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]],
+          ),
+          (
+            "Atlas 0",
+            vec![[0.0, 0.0], [0.5, 0.0], [0.5, 0.5], [0.0, 0.5]],
+          ),
+          (
+            "Atlas 1",
+            vec![[0.0, 0.5], [0.5, 0.5], [0.5, 1.0], [0.0, 1.0]],
+          ),
+          (
+            "Atlas 2",
+            vec![[0.5, 0.0], [1.0, 0.0], [1.0, 0.5], [0.5, 0.5]],
+          ),
+          (
+            "Atlas 3",
+            vec![[0.5, 0.5], [1.0, 0.5], [1.0, 1.0], [0.5, 1.0]],
+          ),
         ],
-        vec![0, 2, 1, 0, 3, 2],
+      )
+      .unwrap();
+    Ok(())
+  }
+  fn exit(
+    &mut self,
+    _command_queue: &mut CommandQueue,
+    _context: &mut Context,
+  ) -> Result<(), EngineError> {
+    Ok(())
+  }
+}
+
+impl AppWindowEventHandler for ExampleApp {
+  fn close_request(
+    &mut self,
+    elwt: &ELWT,
+    _command_queue: &mut CommandQueue,
+    _context: &mut Context,
+  ) -> Result<(), EngineError> {
+    elwt.exit();
+    Ok(())
+  }
+}
+
+/// An example scene.
+pub struct ExampleScene;
+
+impl Scene for ExampleScene {
+  fn load(
+    &mut self,
+    _command_queue: &mut CommandQueue,
+    context: &mut Context,
+  ) -> Result<(), EngineError> {
+    // Spawn the active entity.
+    // Solid red renderable.
+    let entity = context.world.spawn_entity((
+      Transform::new([0.0, 0.0], [128.0, 128.0]),
+      Renderable::new(
+        Color::rgb(1.0, 0.0, 0.0),
+        Texture::none(),
+        Mesh::new(
+          vec![
+            Point::new(-0.5, -0.5),
+            Point::new(0.5, -0.5),
+            Point::new(0.5, 0.5),
+            Point::new(-0.5, 0.5),
+          ],
+          vec![0, 2, 1, 0, 3, 2],
+        ),
       ),
-    ),
-    Camera::new([0.0, 0.0]),
-  ));
-  world.actives.set_camera(entity);
-  // Spawn more entities.
-  more_entities(&mut world);
-  // Run the event loop.
-  event_loop
-    .run(move |event, elwt| match event {
-      Event::AboutToWait => window.request_redraw(),
-      Event::WindowEvent { window_id, event } => {
-        assert_eq!(window.id(), window_id);
-        match event {
-          WindowEvent::RedrawRequested => {
-            requests(&mut renderer);
-            renderer.execute(&mut world).unwrap();
-          },
-          WindowEvent::CloseRequested => elwt.exit(),
-          _ => (),
-        }
-      },
-      _ => (),
-    })
-    .unwrap();
+      Camera::new([0.0, 0.0]),
+    ));
+    context.world.actives.set_camera(entity);
+    // Spawn more entities.
+    more_entities(&mut context.world);
+    Ok(())
+  }
+  fn frame(
+    &mut self,
+    _command_queue: &mut CommandQueue,
+    context: &mut Context,
+  ) -> Result<(), EngineError> {
+    requests(&mut context.renderer);
+    Ok(())
+  }
+  fn unload(
+    &mut self,
+    _command_queue: &mut CommandQueue,
+    _context: &mut Context,
+  ) -> Result<(), EngineError> {
+    Ok(())
+  }
 }
 
 /// Spawn more entities.
@@ -204,4 +243,10 @@ fn requests(renderer: &mut Renderer) {
       ),
     ),
   ));
+  // A blue point.
+  renderer.add_render_request(Basics::point([0.0, 0.0], Color::blue()));
+}
+
+fn main() -> Result<(), EngineError> {
+  Box::new(ExampleApp).run()
 }
