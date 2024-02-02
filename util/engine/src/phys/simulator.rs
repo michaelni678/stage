d504e23f9c456ecb63afa9db5a0e0256015b93cb
michaelni678 <michael.ni678@gmail.com>
2024-02-02 01:35:10 -0500

    Add env coll

--- util/engine/src/phys/simulator.rs
+++ util/engine/src/phys/simulator.rs
@@ -1,4 +1,4 @@
-use crate::CollisionTree;
+use crate::{CollisionTree, Point, Size, World};
 
 /// Simulates physics.
 #[derive(Default)]
@@ -10,5 +10,17 @@ impl Simulator {
   /// Create a new simulator.
   pub fn new() -> Self {
     Self::default()
+  }
+  /// Add an environment collider to the simulator.
+  pub fn add_environment_collider(
+    &mut self, 
+    position: impl Into<Point>, 
+    size: impl Into<Size>,
+  ) -> u64 {
+    self.tree.add_environment_collider(position.into(), size.into())
+  }
+  /// Execute the simulator.
+  pub fn execute(&mut self, world: &mut World) {
+
   }
 }
--- util/engine/src/phys/tree.rs
+++ util/engine/src/phys/tree.rs
@@ -1,14 +1,20 @@
 use rstar::{RTree, RTreeObject};
+use rustc_hash::FxHashMap;
 use crate::{Point, Size, AABB};
 
+/// A tree of colliders.
 pub struct CollisionTree {
   inner: RTree<TreeObject>,
+  environment_colliders: FxHashMap<u64, TreeObject>,
+  next_id: u64,
 }
 
 impl Default for CollisionTree {
   fn default() -> Self {
     Self {
       inner: RTree::new(),
+      environment_colliders: FxHashMap::default(),
+      next_id: 0,
     }
   }
 }
@@ -18,11 +24,35 @@ impl CollisionTree {
   pub fn new() -> Self {
     Self::default()
   }
+  /// Generate a new id.
+  pub fn generate_id(&mut self) -> u64 {
+    let id = self.next_id;
+    self.next_id += 1;
+    id
+  }
+  /// Add an environment collider to the tree.
+  pub fn add_environment_collider(&mut self, position: Point, size: Size) -> u64 {
+    // Generate an id for the environment collider.
+    let id = self.generate_id();
+    // Create the tree object.
+    let obj = TreeObject {
+      position: position,
+      size: size,
+      source: TreeObjectSource::Environment,
+    };
+    // Add to the tree and `environment_colliders`.
+    self.environment_colliders.insert(id, obj);
+    self.inner.insert(obj);
+    id
+  }
 }
 
+/// An object that is insertable into the collision tree.
+#[derive(Copy, Clone)]
 pub struct TreeObject {
   position: Point,
   size: Size,
+  source: TreeObjectSource,
 }
 
 impl RTreeObject for TreeObject {
@@ -30,4 +60,10 @@ impl RTreeObject for TreeObject {
   fn envelope(&self) -> Self::Envelope {
     AABB::from_corners(self.position, self.position + self.size)
   }
+}
+
+/// Tree object source.
+#[derive(Copy, Clone)]
+pub enum TreeObjectSource {
+  Environment,
 }
